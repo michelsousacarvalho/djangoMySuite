b6665059e7b0534a94d03629a76173f6ce92e026
Michel Pai <michelsousacarvalho@gmail.com>
2015-01-25 20:16:12 -0200

    create urls

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+from django.utils import timezone
+import datetime
+
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
+        
+    def __unicode__(self):  
+        return u'%s , %s' % (self.question_text, self.pub_date) 
+        
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now()
     
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_question = models.CharField(max_length = 200)
     votes = models.IntegerField(default=0)
+    
+    def __unicode__(self):  
+        return u'%s' % (self.choice_question)
